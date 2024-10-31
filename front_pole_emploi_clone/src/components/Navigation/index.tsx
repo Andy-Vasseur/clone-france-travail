aaa03ed9146d1f6d9c862828ecd3b783bbfed936
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-10-31 01:38:59 +0100

    :sparkles: ajout de la barre de Navigation.tsx, sans les dropdown et la pub

--- front_pole_emploi_clone/src/components/Navigation/index.tsx
+++ front_pole_emploi_clone/src/components/Navigation/index.tsx
@@ -1,25 +1,72 @@
-// Assets
+// Imports
+import { Link } from "react-router-dom";
+
+// Logos
 import MarianneLogo from "/svg/marianne.svg";
 import FranceTravailLogo from "/svg/france-travail.svg";
 
+// Icons
+import MenuIcon from "/icons/menu.svg";
+import CloseIcon from "/icons/close.svg";
+import UserIcon from "/icons/user.svg";
+import SearchIcon from "/icons/search.svg";
+
 function Navigation() {
+  const handleButtonClick = () => {
+    console.log("Button clicked");
+  };
+
   return (
-    <nav>
-      <div>
-        <span>
-          <img src={MarianneLogo} alt="Logo de la Marianne" />
+    <nav className="flex justify-between items-center p-4 max-h-20 text-black bg-white">
+      <div className="flex justify-center items-center">
+        <span className="p-1 w-[61px] h-full">
+          <img
+            src={MarianneLogo}
+            alt="Logo de la Marianne"
+            className="w-full h-full"
+          />
         </span>
-        <span>
-          <img src={FranceTravailLogo} alt="Logo de France travail" />
-        </span>
-        <div>Menu</div>
+        <Link
+          to="/"
+          className="ml-4 h-11"
+          title="Retourner à l'accueil de France Travail"
+        >
+          <img
+            src={FranceTravailLogo}
+            alt="Logo de France travail"
+            className="w-full h-full"
+          />
+        </Link>
+        <div className="flex justify-center items-center h-14 border-l-[1px]">
+          <button
+            className="flex items-center ml-2 text-xs uppercase"
+            onClick={handleButtonClick}
+          >
+            <span className="flex justify-center items-center p-1 w-8 h-8">
+              <img src={MenuIcon} alt="Menu" className="w-8 h-8" />
+            </span>
+            Menu
+          </button>
+        </div>
       </div>
 
-      <div>{/* ad */}</div>
+      <div className="flex justify-center items-center w-[467px] border-2 border-black">
+        <h3>This is a ad, please don&apos;t mind it.</h3>
+      </div>
 
-      <div>
-        <div>Connexion</div>
-        <div>Rechercher</div>
+      <div className="flex justify-center items-center space-x-6">
+        <button className="flex justify-center items-center h-14 text-xs uppercase">
+          <span>
+            <img src={UserIcon} alt="User" className="w-8 h-8" />
+          </span>
+          Connexion
+        </button>
+        <button className="flex justify-center items-center h-14 text-xs uppercase">
+          <span>
+            <img src={SearchIcon} alt="Search" className="w-8 h-8" />
+          </span>
+          Rechercher
+        </button>
       </div>
     </nav>
   );

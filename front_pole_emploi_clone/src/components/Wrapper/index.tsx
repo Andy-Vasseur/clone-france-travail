df0150c9e361a831b78e28d347b01109101fb5e0
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-11-02 17:35:00 +0100

    :sparkles: finish the V1 frontend of the Wrapper.tsx

--- front_pole_emploi_clone/src/components/Wrapper/index.tsx
+++ front_pole_emploi_clone/src/components/Wrapper/index.tsx
@@ -11,104 +11,86 @@ import SearchIcon from "/icons/search-white.svg";
 
 function Wrapper() {
   const bottomLinks = [
+    { icon: "/icons/sync.svg", text: "M'actualiser" },
     {
-      icon: "/icons/sync.svg",
-      text: "Allocations et aides",
+      icon: "/icons/espace-personnel.svg",
+      text: "Gérer mon dossier, Publier mon CV",
     },
     {
-      icon: "",
-      text: "Allocations et aides",
+      icon: "/icons/inscription.svg",
+      text: "M'inscrire, me réinscrire (demandeur d'emploi)",
     },
+    { icon: "/icons/documents.svg", text: "Allocations et aides" },
+    { icon: "/icons/stack.svg", text: "Trouver ma formation" },
     {
-      icon: "",
-      text: "Allocations et aides",
+      icon: "/icons/emploi-store.svg",
+      text: "Explorer les services en ligne de l'emploi",
     },
-    {
-      icon: "",
-      text: "Allocations et aides",
-    },
-    {
-      icon: "",
-      text: "Allocations et aides",
-    },
-    {
-      icon: "",
-      text: "Allocations et aides",
-    },
-    {
-      icon: "",
-      text: "Allocations et aides",
-    },
+    { icon: "/icons/dots.svg", text: "Plus de services" },
   ];
 
   return (
-    <div className="relative h-[447px] bg-red-800">
+    <div className="relative h-[447px]">
       {/* Background picture */}
       <div className="absolute w-full h-full">
         <img src={Banner} alt="banner" className="w-full h-full object-cover" />
       </div>
-      <div className="absolute text-center z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+
+      {/* Main content */}
+      <div className="absolute text-center z-10 top-[15%] left-1/2 transform -translate-x-1/2 w-full max-w-[850px] px-4">
         {/* Number of offers available */}
         <h1 className="text-4xl font-semibold text-white drop-shadow-md">
-          1 156 252 offres d&apos;emploi disponibles
+          1 156 252 offres d'emploi disponibles
         </h1>
+
         {/* Search inputs */}
         <form
           method="POST"
-          className="flex justify-between items-center mt-8 mb-4 w-full h-14 rounded-[4px] bg-white"
+          className="flex justify-center items-center mt-8 mb-4 max-w-[800px] mx-auto h-14 rounded-md bg-white shadow-lg overflow-hidden"
         >
-          <div className="flex items-center py-2 px-3 w-[280px] h-12 text-[.8rem]">
-            <span>
-              <img src={WorkIcon} alt="work" className="w-6 h-6" />
-            </span>
+          <div className="flex items-center w-1/2 h-full px-3 border-r border-gray-200">
+            <img src={WorkIcon} alt="work" className="w-5 h-5 mr-2" />
             <input
               type="text"
               placeholder="Métier, compétence, mot-clé, n° d'offre"
-              className="p-2 w-80 h-12 focus:outline-none"
+              className="flex-grow text-sm focus:outline-none"
             />
           </div>
-          <div className="flex items-center py-2 px-3 w-[280px] h-12 text-[.8rem] border-l-[1px]">
-            <span>
-              <img src={LocationIcon} alt="location" className="w-6 h-6" />
-            </span>
+          <div className="flex items-center w-1/2 h-full px-3">
+            <img src={LocationIcon} alt="location" className="w-5 h-5 mr-2" />
             <input
               type="text"
               placeholder="Lieu de travail"
-              className="p-2 w-80 h-12 focus:outline-none"
+              className="flex-grow text-sm focus:outline-none"
             />
           </div>
           <button
             type="submit"
-            className="p-2 w-|55px] h-[56px] text-sm rounded-tr-[4px] rounded-br-[4px] shadow-lg bg-[#FF5950] transition-colors duration-300 ease-in-out hover:bg-[#FF291D]"
+            className="flex items-center justify-center w-14 h-full bg-[#FF5950] hover:bg-[#FF291D] transition duration-300"
           >
-            <span>
-              <img src={SearchIcon} alt="search" className="w-full h-full" />
-            </span>
+            <img src={SearchIcon} alt="search" className="w-10 h-10" />
           </button>
         </form>
-        {/* Redirection Link */}
-        <Link
-          to="#"
-          className="text-white text-[.8rem] font-medium  hover:underline"
-        >
-          Rechercher des offres par région ou par secteur d&apos;activité
-        </Link>
 
-        {/* Bottom buttons */}
-        <div className="flex justify-center items-center mt-8">
+        {/* Redirection Link */}
+        <Link to="#" className="text-white text-sm font-medium hover:underline">
+          Rechercher des offres par région ou par secteur d'activité
+        </Link>
+      </div>
+
+      {/* Bottom buttons */}
+      <div className="absolute bottom-0 w-full py-4 bg-fade-to-top">
+        <div className="flex justify-center space-x-6 max-w-5xl mx-auto">
           {bottomLinks.map((link, index) => (
             <Link
               to="#"
               key={index}
-              className="p-3 w-[84px] h-[84px] rounded-full bg-[#FF5950] hover:animate-spin-slow"
+              className="flex flex-col items-center text-center text-white group rounded-full"
             >
-              <span>
-                <img
-                  src={link.icon}
-                  alt="sync"
-                  className="w-full h-full rotate-90 scale-x-[-1]"
-                />
-              </span>
+              <div className="w-20 h-20 flex items-center justify-center rounded-full bg-[#FF5950] group-hover:scale-105 transition transform duration-300">
+                <img src={link.icon} alt={link.text} className="w-12 h-12" />
+              </div>
+              <span className="mt-2 text-xs w-32 text-center">{link.text}</span>
             </Link>
           ))}
         </div>
--- front_pole_emploi_clone/tailwind.config.js
+++ front_pole_emploi_clone/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "fade-to-top":
+          "linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))",
+      },
       animation: {
         "spin-slow": "spin 1.5s linear infinite",
       },
